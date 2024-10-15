b66a3b475323398c035d80174bf65c49e7d37e27
Jason Chadwick <jchadwick100@gmail.com>
2024-10-15 14:35:34 -0400

    more comprehensive tests, including simple multi-merge

--- tests/test_window_manager.py
+++ tests/test_window_manager.py
@@ -16,6 +16,7 @@ to test:
     ...
 """
 import numpy as np
+import networkx as nx
 from swiper2.simulator import DecodingSimulator
 from swiper2.lattice_surgery_schedule import LatticeSurgerySchedule
 
@@ -25,9 +26,11 @@ idle_schedule.discard([(0,0)])
 
 merge_schedule = LatticeSurgerySchedule()
 merge_schedule.idle([(0,0)], 10)
-merge_schedule.idle([(0,100)], 10)
-merge_schedule.merge([(0,0), (0,100)], [(0,i) for i in range(1,100)])
-merge_schedule.discard([(0,0), (0,100)])
+merge_schedule.idle([(0,10)], 10)
+merge_schedule.merge([(0,0), (0,10)], [(0,i) for i in range(1,10)])
+merge_schedule.merge([(0,0), (0,10)], [(0,i) for i in range(1,10)])
+merge_schedule.merge([(0,0), (0,10)], [(0,i) for i in range(1,10)])
+merge_schedule.discard([(0,0), (0,10)])
 
 def test_sliding_idle():
    """Test that SlidingWindowManager can correctly handle idle rounds."""
@@ -37,7 +40,6 @@ def test_sliding_idle():
    # Check windows as they are released
    while not simulator.is_done():
       simulator.step_experiment()
-      # TODO: check windows as they come out
 
       for window_idx, window in enumerate(simulator._window_manager.all_windows):
          assert window.constructed or window_idx in simulator._window_manager.window_buffer_wait
@@ -47,6 +49,7 @@ def test_sliding_idle():
    # Check final windows
    device_data, window_data, decoding_data = simulator.get_data()
    device_rounds_covered = np.full(device_data.num_rounds, -1, dtype=int)
+   assert nx.is_directed_acyclic_graph(window_data.window_dag)
    assert set(window_data.window_dag.nodes) == set(range(len(window_data.all_windows)))
    for i,window in enumerate(window_data.all_windows):
       assert len(window.commit_region) == 1
@@ -72,7 +75,6 @@ def test_parallel_idle():
    # Check windows as they are released
    while not simulator.is_done():
       simulator.step_experiment()
-      # TODO: check windows as they come out
 
       for window_idx, window in enumerate(simulator._window_manager.all_windows):
          assert window.constructed or window_idx in simulator._window_manager.window_buffer_wait
@@ -82,7 +84,10 @@ def test_parallel_idle():
    source_indices = simulator._window_manager.source_indices
    sink_indices = simulator._window_manager.sink_indices
    device_rounds_covered = np.full(device_data.num_rounds, -1, dtype=int)
+   assert nx.is_directed_acyclic_graph(window_data.window_dag)
    assert set(window_data.window_dag.nodes) == set(range(len(window_data.all_windows)))
+   assert nx.is_bipartite(window_data.window_dag)
+   assert len(list(nx.topological_generations(window_data.window_dag))) == 2
    for i,window in enumerate(window_data.all_windows):
       if i < len(window_data.all_windows)-1:
          assert len(window.commit_region) == 1 or len(window.commit_region) == 3
@@ -141,31 +146,62 @@ def test_parallel_idle():
    # that this test passes
    assert np.all(device_rounds_covered >= 0)
 
-def test_parallel_regularT():
-   """Test that ParallelWindowManager can correctly handle a regular T schedule."""
+def test_sliding_merge():
+   """Test that SlidingWindowManager can correctly handle a merge schedule."""
    simulator = DecodingSimulator(7, lambda _: 14, 2, 0, speculation_mode='separate')
-   simulator.initialize_experiment(merge_schedule, 'parallel', False)
+   simulator.initialize_experiment(merge_schedule, 'sliding', False)
 
    # Check windows as they are released
    while not simulator.is_done():
       simulator.step_experiment()
-      # for window_idx, window in enumerate(simulator._window_manager.all_windows):
-      #    assert window.constructed or window_idx in simulator._window_manager.window_buffer_wait
+      for window_idx, window in enumerate(simulator._window_manager.all_windows):
+         assert window.constructed or window_idx in simulator._window_manager.window_buffer_wait
 
    device_data, window_data, decoding_data = simulator.get_data()
-   # assert set(window_data.window_dag.nodes) ==
-   # set(range(len(window_data.all_windows)))
+   assert nx.is_directed_acyclic_graph(window_data.window_dag)
+   assert set(window_data.window_dag.nodes) == set(range(len(window_data.all_windows)))
+   for i,window in enumerate(window_data.all_windows):
+      assert len(window.commit_region) == 1
+      cr = window.commit_region[0]
+      merge_instr = window.merge_instr
+      if len(merge_instr) == 2:
+         assert cr.patch == (0,0) or cr.patch == (0,10)
+      # Check that only connections between the two merges are on the ends
+      if cr.patch not in [(0,0), (0,10)]:
+         assert len(merge_instr) == 1
+         # Commit region is somewhere in merge ancilla region
+         dag_neighbors = set(window_data.window_dag.successors(i)) | set(window_data.window_dag.predecessors(i))
+         print((cr.patch, cr.round_start), [(window_data.all_windows[neighbor].commit_region[0].patch, window_data.all_windows[neighbor].commit_region[0].round_start) for neighbor in dag_neighbors])
+         assert len(dag_neighbors) == 2
+         for neighbor in dag_neighbors:
+            cr_n = window_data.all_windows[neighbor].commit_region[0]
+            assert abs(cr_n.patch[1] - cr.patch[1]) == 1
+            merges = window_data.all_windows[neighbor].merge_instr
+            assert merges == merge_instr or cr_n.patch[1] in [0, 10]
+
+def test_parallel_merge():
+   """Test that ParallelWindowManager can correctly handle a merge schedule."""
+   simulator = DecodingSimulator(7, lambda _: 14, 2, 0, speculation_mode='separate')
+   simulator.initialize_experiment(merge_schedule, 'parallel', False)
+
+   while not simulator.is_done():
+      simulator.step_experiment()
+      for window_idx, window in enumerate(simulator._window_manager.all_windows):
+         assert window.constructed or window_idx in simulator._window_manager.window_buffer_wait
+
+   device_data, window_data, decoding_data = simulator.get_data()
+   assert nx.is_directed_acyclic_graph(window_data.window_dag)
+   assert set(window_data.window_dag.nodes) == set(range(len(window_data.all_windows)))
+   assert nx.is_bipartite(window_data.window_dag)
+   assert len(list(nx.topological_generations(window_data.window_dag))) == 2
    
    # # Check that no source-source or sink-sink conflicts exist
-   # merge_windows = get_merge_windows()
-   # for _, window_idxs in merge_windows.items():
-   #       for i, window_idx_1 in enumerate(window_idxs):
-   #          for window_idx_2 in window_idxs[:i]:
-   #             for patch1 in [cr.patch for cr in self.all_windows[window_idx_1].commit_region]:
-   #                   for patch2 in [cr.patch for cr in self.all_windows[window_idx_2].commit_region]:
-   #                      if (abs(patch1[0] - patch2[0]) + abs(patch1[1] - patch2[1]) == 1):
-   #                         if (window_idx_1 in self.sink_indices and window_idx_2 in self.sink_indices) or (window_idx_1 in self.source_indices and window_idx_2 in self.source_indices):
-   #                               raise ValueError("Failed to resolve sink-sink or source-source conflict")
+   # for window_idx_1, window_1 in enumerate(window_data.all_windows):
+   #    for window_idx_2, window_2 in enumerate(window_data.all_windows[:window_idx_1]):
+   #       for patch1 in [cr.patch for cr in window_1.commit_region]:
+   #             for patch2 in [cr.patch for cr in window_2.commit_region]:
+   #                if (abs(patch1[0] - patch2[0]) + abs(patch1[1] - patch2[1]) == 1):
+   #                   assert (window_idx_1 in simulator._window_manager.sink_indices and window_idx_2 in simulator._window_manager.source_indices) or (window_idx_1 in simulator._window_manager.source_indices and window_idx_2 in simulator._window_manager.sink_indices)
 
 if __name__ == '__main__':
-   test_parallel_regularT()
+   test_parallel_merge()
