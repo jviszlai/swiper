eef39150913203276eb7cc88bf77b97276742f7e
Jason Chadwick <jasoncha@andrew.cmu.edu>
2024-12-04 13:40:17 -0600

    new job

--- slurm/submit_jobs.py
+++ slurm/submit_jobs.py
@@ -13,10 +13,10 @@ if __name__ == '__main__':
     cur_time = dt.datetime.now()
 
     # USER SETTING: maximum job duration
-    max_time = dt.timedelta(hours=24*4)
+    max_time = dt.timedelta(hours=36)
 
     if max_time.days > 0:
-        assert max_time.days <= 7
+        assert max_time.days == 1
         max_time_str = f'1-{max_time.seconds // 3600:02d}:{(max_time.seconds % 3600) // 60:02d}:{max_time.seconds % 60:02d}'
     else:
         max_time_str = f'{max_time.seconds // 3600:02d}:{(max_time.seconds % 3600) // 60:02d}:{max_time.seconds % 60:02d}'
@@ -47,19 +47,12 @@ if __name__ == '__main__':
         benchmark_info = {row['']:row for row in reader}
 
     # Can make a chosen smaller list of these instead
-    keep_files = ['qft_30', 'grover_ancilla_9', 'electronic_structure', 'heisenberg_5', 'qpeexact_20', 'qft_20', 'heisenberg_3']
     benchmark_files = []
     benchmark_names = {}
     memory_settings = None
     for file in os.listdir('benchmarks/cached_schedules/'):
         # USER SETTING: filter benchmark files if desired
-        if file.endswith('.lss'):
-            keep = False
-            for keep_f in keep_files:
-                if keep_f in file:
-                    keep = True
-            if not keep:
-                continue
+        if file.endswith('.lss') and not file.startswith('memory') and not file.startswith('regular') and not file.startswith('random'):
             path = os.path.join('benchmarks/cached_schedules/', file)
             newpath = os.path.join(benchmark_dir, file)
             # copy files to data dir to preserve them
@@ -82,12 +75,12 @@ if __name__ == '__main__':
         'distance':[21],
         'scheduling_method':['sliding', 'parallel', 'aligned'],
         'decoder_latency_or_dist_filename':[decoder_dist_filename],
-        'speculation_mode':['separate', None],
+        'speculation_mode':['separate'],
         'speculation_latency':[1],
         'speculation_accuracy':[0.9],
         'poison_policy':['successors'],
         'missed_speculation_modifier':[1.4],
-        'max_parallel_processes':[None, 'predict'],
+        'max_parallel_processes':['predict'],
         'rng':list(range(10)),
         'lightweight_setting':[2],
     }
@@ -125,7 +118,7 @@ if __name__ == '__main__':
         configs_by_mem.setdefault(mem_gb, []).append(i)
 
     # USER SETTING: submission delay (if too many jobs at once)
-    submission_delay = dt.timedelta(hours=1)
+    submission_delay = dt.timedelta(hours=0)
     last_submit_time = None
     max_tasks_per_job = 800
     job_ids = []
@@ -144,17 +137,16 @@ if __name__ == '__main__':
 #SBATCH --job-name={cur_time.strftime("%Y%m%d_%H%M%S")}
 #SBATCH --output={log_dir}/%a.out
 #SBATCH --error={log_dir}/%a.out
-#SBATCH --partition=fast-long
+#SBATCH --account=pi-ftchong
+#SBATCH --partition=caslake
 #SBATCH --array={','.join([str(x) for x in selected_config_indices])}
 #SBATCH --time={max_time_str}
 #SBATCH --ntasks=1
 #SBATCH --mem-per-cpu={mem_gb*1000}
 
+module load python
 eval "$(conda shell.bash hook)"
-conda activate /home/viszlai/anaconda3/envs/pySwiper/
-
-cd /home/viszlai/slurm/swiper
-
+conda activate /project/ftchong/projects/envs/pySwiper/
 
 python slurm/run_simulation.py "{config_filename}" "{output_dir}" {int(max_time.total_seconds())}'''
                 )
